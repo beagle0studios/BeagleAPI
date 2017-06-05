00137930bb491e99f15f2a553d02f431dc0dced6
Sebastian Kraatz <sebastian.kraatz@yahoo.de>
2017-06-05 18:44:23 +0200

    Added class "Predefined" where users can access methods and call them via a delegate for example.

---
+++ Misc/ActionMethods/Predefined.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeagleAPI.Misc.ActionMethods
+{
+    public sealed class Predefined
+    {
+        public void CreateFormWithinPanel(Window.StdW.StdWin form, System.Windows.Forms.Panel panel)
+        {
+            form.TopLevel = false;
+            form.AutoScroll = true;
+            panel.Controls.Add(form);
+            form.Show();
+        }
+    }
+}
