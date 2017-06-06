9fbeb5b4608bec530214e725c6bc458564d0be76
Sebastian Kraatz <sebastian.kraatz@yahoo.de>
2017-06-06 09:54:55 +0200

    Modified ManagerDisplay tohave more functions like getting process info or stop a task TODO! in code

--- Window/Manager/ManagerDisplay.Designer.cs
+++ Window/Manager/ManagerDisplay.Designer.cs
@@ -29,37 +29,76 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonE2 = new BeagleAPI.Controls.ButtonE();
+            this.buttonE1 = new BeagleAPI.Controls.ButtonE();
             ((System.ComponentModel.ISupportInitialize)(this.windowLogoIcon)).BeginInit();
             this.pnl_titlebar_exstdwin.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnl_titlebar_exstdwin
             // 
-            this.pnl_titlebar_exstdwin.Size = new System.Drawing.Size(783, 31);
+            this.pnl_titlebar_exstdwin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnl_titlebar_exstdwin.Size = new System.Drawing.Size(482, 20);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 31;
-            this.listBox1.Location = new System.Drawing.Point(3, 50);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(2, 32);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(768, 252);
+            this.listBox1.Size = new System.Drawing.Size(474, 164);
             this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // buttonE2
+            // 
+            this.buttonE2.Enabled = false;
+            this.buttonE2.FlatAppearance.BorderSize = 0;
+            this.buttonE2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonE2.GetParent = null;
+            this.buttonE2.Location = new System.Drawing.Point(252, 201);
+            this.buttonE2.Name = "buttonE2";
+            this.buttonE2.Size = new System.Drawing.Size(126, 29);
+            this.buttonE2.TabIndex = 5;
+            this.buttonE2.Text = "Get Process Info";
+            this.buttonE2.UseVisualStyleBackColor = true;
+            this.buttonE2.Click += new System.EventHandler(this.buttonE2_Click);
+            // 
+            // buttonE1
+            // 
+            this.buttonE1.Enabled = false;
+            this.buttonE1.FlatAppearance.BorderSize = 0;
+            this.buttonE1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonE1.GetParent = null;
+            this.buttonE1.Location = new System.Drawing.Point(384, 201);
+            this.buttonE1.Name = "buttonE1";
+            this.buttonE1.Size = new System.Drawing.Size(92, 29);
+            this.buttonE1.TabIndex = 4;
+            this.buttonE1.Text = "Stop Task";
+            this.buttonE1.UseVisualStyleBackColor = true;
+            this.buttonE1.Click += new System.EventHandler(this.buttonE1_Click);
             // 
             // ManagerDisplay
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 313);
+            this.ClientSize = new System.Drawing.Size(482, 241);
+            this.Controls.Add(this.buttonE2);
+            this.Controls.Add(this.buttonE1);
             this.Controls.Add(this.listBox1);
+            this.CreationTime = new System.DateTime(2017, 6, 6, 9, 46, 16, 67);
             this.Location = new System.Drawing.Point(10, 10);
-            this.Margin = new System.Windows.Forms.Padding(9, 12, 9, 12);
+            this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.Name = "ManagerDisplay";
+            this.ShowCloseButton = true;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Title = "Manager Display";
             this.WINDOW_TITLE = "Manager Display";
             this.Controls.SetChildIndex(this.pnl_titlebar_exstdwin, 0);
             this.Controls.SetChildIndex(this.listBox1, 0);
+            this.Controls.SetChildIndex(this.buttonE1, 0);
+            this.Controls.SetChildIndex(this.buttonE2, 0);
             ((System.ComponentModel.ISupportInitialize)(this.windowLogoIcon)).EndInit();
             this.pnl_titlebar_exstdwin.ResumeLayout(false);
             this.pnl_titlebar_exstdwin.PerformLayout();
@@ -70,5 +109,7 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private Controls.ButtonE buttonE2;
+        private Controls.ButtonE buttonE1;
     }
 }
--- Window/Manager/ManagerDisplay.cs
+++ Window/Manager/ManagerDisplay.cs
@@ -12,11 +12,15 @@ namespace BeagleAPI.Window.Manager
 {
     public partial class ManagerDisplay : StdW.StdWin
     {
+        private int currentSelection = -1;
+        private Dictionary<string, IStdWin> windows;
+
         public ManagerDisplay()
         {
             InitializeComponent();
             WindowManager.WhenRegisterWindow += WindowManager_WhenRegisterWindow;
             WindowManager.WhenUnregisterWindow += WindowManager_WhenUnregisterWindow;
+            windows = new Dictionary<string, IStdWin>();
         }
 
         private void WindowManager_WhenUnregisterWindow(object sender, SysDebug.DelegatesEvents.StdWinUnregisterEventArgs e)
@@ -27,6 +31,58 @@ namespace BeagleAPI.Window.Manager
         private void WindowManager_WhenRegisterWindow(object sender, SysDebug.DelegatesEvents.StdWinRegisterEventArgs e)
         {
             listBox1.Items.Add(e.Name + " | " + e.window.WINDOW_TITLE);
+            windows.Add(e.Name, e.window);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            currentSelection = listBox1.SelectedIndex;
+
+            //if nothing was selected
+            if (currentSelection < 0)
+            {
+                buttonE1.Enabled = false;
+                buttonE2.Enabled = false;
+            }
+            else
+            {
+                buttonE1.Enabled = true;
+                buttonE2.Enabled = true;
+            }
+        }
+
+        private void buttonE1_Click(object sender, EventArgs e)
+        {
+            IStdWin win;
+            windows.TryGetValue("", out win); //TODO: insert a valid string to detect window
+
+            //if win is taskman, abort operation. dont close taskman via taskman
+            if (win == this)
+                return;
+
+            foreach (var item in ManageHelper.window_collection)
+            {
+                if (item.Value.WINDOW_TITLE == win.WINDOW_TITLE)
+                {
+                    item.Value.Close();
+                    //listBox1.Items[currentSelection] += " (Closed At: " + DateTime.Now.ToString() + ")";
+                    listBox1.Items.RemoveAt(currentSelection);
+                    break;
+                }
+            }
+        }
+
+        private void buttonE2_Click(object sender, EventArgs e)
+        {
+            IStdWin win;
+            windows.TryGetValue("", out win); //TODO: insert a valid string to detect window
+
+            Window.Message.Messages m = new Window.Message.Messages(
+                "Process Info",
+                "Window Title: " + win.WINDOW_TITLE + Environment.NewLine +
+                "Registered Name: " + win.ToString() + Environment.NewLine +
+                "Creation Time: " + win.CreationTime.ToString(), true);
         }
     }
 }
