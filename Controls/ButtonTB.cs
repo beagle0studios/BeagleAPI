9a6b2570797e4bbecdf9a7375979194a43af1b96
Sebastian Kraatz <sebastian.kraatz@yahoo.de>
2017-06-08 11:32:31 +0200

    Added ButtonTB for providing a button to taskbar with extended functionality

---
+++ Controls/ButtonTB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BeagleAPI.Controls
+{
+    public class ButtonTB : ButtonE
+    {
+        public ButtonTB(BeagleAPI.Window.IStdWin parent, Icon icon, string windowID) : base(parent)
+        {
+            BackgroundImage = icon.ToBitmap();
+            BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            INFORMATION = windowID;
+            Size = new Size(30, 30);
+        }
+    }
+}
