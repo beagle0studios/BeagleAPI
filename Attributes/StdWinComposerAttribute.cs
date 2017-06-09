fa7401aedd5df606ceb15bef1ef698393362e8d5
Sebastian Kraatz <sebastian.kraatz@yahoo.de>
2017-06-09 14:32:05 +0200

    Added StdWinComposerAttribute. Anwender müssen dieses Attribut in ihre eigenen StdWin-Ableitungen setzen, damit BeagleAPI es später verarbeiten kann.

---
+++ Attributes/StdWinComposerAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeagleAPI.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public sealed class StdWinComposerAttribute : Attribute
+    {
+        private string _windowname;
+        private long _windowid = 0;
+
+        public long WindowID
+        {
+            get { return _windowid; }
+            set { _windowid = value; }
+        }
+
+        public StdWinComposerAttribute (string WindowName)
+        {
+            _windowname = WindowName;
+        }
+    }
+}
